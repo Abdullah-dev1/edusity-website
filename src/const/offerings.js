6a2065b155a3dabc19bc198f4fb4c1db6102d77d
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-15 01:57:10 +0500

    :white_check_mark: ADD: add const file

---
+++ src/const/offerings.js
@@ -0,0 +1,29 @@
+import IMG1 from "../assets/offering1.png";
+import IMG2 from "../assets/offering2.png";
+import IMG3 from "../assets/offering3.png";
+import LOGO1 from "../assets/offering-logo1.png";
+import LOGO2 from "../assets/offering-logo2.png";
+import LOGO3 from "../assets/offering-logo3.png";
+
+const galleryCard = [
+    {
+        id: 1,
+        image: IMG1,
+        caption: "Graduation Degree",
+        logo: LOGO1
+    },
+    {
+        id: 2,
+        image: IMG2,
+        caption: "Masters Degree",
+        logo: LOGO2
+    },
+    {
+        id: 3,
+        image: IMG3,
+        caption: "Post Graduation",
+        logo: LOGO3
+    }
+]
+
+export default galleryCard;
