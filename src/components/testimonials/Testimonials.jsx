fb630b9a034abd81fd9f53edf464f09ddfd456c7
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-16 02:38:16 +0500

    :tada: FINISH: finish the testimonials component

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,7 +1,60 @@
+import { useState } from "react";
 import "./testimonials.css";
+import testimonialSlide from "../../const/testimonials";
+import ICON1 from "../../assets/next-icon.png";
+import ICON2 from "../../assets/back-icon.png";
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  const [translateX, setTranslateX] = useState(0);
+
+  const handleNext = () => {
+    setTranslateX((prev) => Math.max(prev - 25, -50));
+  };
+
+  const handleBack = () => {
+    setTranslateX((prev) => Math.min(prev + 25, 0));
+  };
+
+  return (
+    <section id="testimonials">
+      <h5>TESTIMONIALS</h5>
+      <h2>What Student Says</h2>
+      <div className="testimonials">
+        <img
+          src={ICON1}
+          alt="next icon"
+          className="next__icon"
+          onClick={handleNext}
+        />
+        <img
+          src={ICON2}
+          alt="back icon"
+          className="back__icon"
+          onClick={handleBack}
+        />
+        <div className="slider">
+          <ul style={{ transform: `translateX(${translateX}%)` }}>
+            {testimonialSlide.map(
+              ({ id, image, name, city, feedback, alter }) => (
+                <li key={id}>
+                  <div className="slide">
+                    <div className="user__info">
+                      <img src={image} alt={alter} />
+                      <div>
+                        <h3>{name}</h3>
+                        <span>{city}</span>
+                      </div>
+                    </div>
+                    <p>{feedback}</p>
+                  </div>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
---
+++ src/const/testimonials.js
@@ -0,0 +1,41 @@
+import SLIDE1 from "../assets/avatar1.png"
+import SLIDE2 from "../assets/avatar2.png"
+import SLIDE3 from "../assets/avatar3.png"
+import SLIDE4 from "../assets/avatar4.png"
+
+const testimonialSlide = [
+    {
+        id: 1,
+        image: SLIDE1,
+        name: "Emily Williams",
+        city: "Edusity, USA",
+        feedback: "Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.",
+        alter: "slide1",
+    },
+    {
+        id: 2,
+        image: SLIDE2,
+        name: "William Jackson",
+        city: "Edusity, USA",
+        feedback: "Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.",
+        alter: "slide2",
+    },
+    {
+        id: 3,
+        image: SLIDE3,
+        name: "Emily Williams",
+        city: "Edusity, USA",
+        feedback: "Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.",
+        alter: "slide3",
+    },
+    {
+        id: 4,
+        image: SLIDE4,
+        name: "William Jackson",
+        city: "Edusity, USA",
+        feedback: "Choosing to pursue my degree at Edusity was one of the best decisions I've ever made. The supportive community, state-of-the-art facilities, and commitment to academic excellence have truly exceeded my expectations.",
+        alter: "slide4",
+    }
+]
+
+export default testimonialSlide;
