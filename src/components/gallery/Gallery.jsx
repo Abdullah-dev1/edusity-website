ccc23489aeb139ef6f32b6c386287858ea41dedf
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-15 20:27:19 +0500

    :tada: FINISH: finish the gallery component

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,7 +1,30 @@
 import "./gallery.css";
+import galleryArray from "../../const/gallery";
+import { HiOutlineArrowLongRight } from "react-icons/hi2";
 
 const Gallery = () => {
-  return <section id="gallery">Gallery</section>;
+  return (
+    <section id="gallery">
+      <div className="container">
+        <div className="title">
+          <h5>GALLERY</h5>
+          <h2>Campus Photos</h2>
+        </div>
+
+        <div className="campus">
+          <div className="gallery">
+            {galleryArray.map(({ id, image, alter }) => (
+              <img src={image} alt={alter} key={id} />
+            ))}
+          </div>
+
+          <button className="btn btn__dark">
+            See more here <HiOutlineArrowLongRight className="btn__icon" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Gallery;
---
+++ src/const/gallery.js
@@ -0,0 +1,29 @@
+import IMAGE1 from "../assets/gallery1.png"
+import IMAGE2 from "../assets/gallery2.png"
+import IMAGE3 from "../assets/gallery3.png"
+import IMAGE4 from "../assets/gallery4.png"
+
+const galleryImgs = [
+    {
+        id: 1,
+        image: IMAGE1,
+        alter: "gallery image 1",
+    },
+    {
+        id: 2,
+        image: IMAGE2,
+        alter: "gallery image 2",
+    },
+    {
+        id: 3,
+        image: IMAGE3,
+        alter: "gallery image 3",
+    },
+    {
+        id: 4,
+        image: IMAGE4,
+        alter: "gallery image 4",
+    }
+]
+
+export default galleryImgs;
