da05e68123f1e57691fb208426b10fc3790181fd
Muhammad Abdullah <mabdullah.dev1@gmail.com>
2024-11-16 16:44:42 +0500

    :tada: FINISH: finish footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,32 @@
 import "./footer.css";
 const Footer = () => {
-  return <section id="footer">Footer</section>;
+  return (
+    <section id="footer">
+      <div className="footer">
+        <p>© 2024 Edusity. All rights reserved.</p>
+        <ul>
+          <li>
+            <a
+              href="https://www.youtube.com/@GreatStackDev/videos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Terms of Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/@GreatStackDev/videos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Privacy Policy
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
